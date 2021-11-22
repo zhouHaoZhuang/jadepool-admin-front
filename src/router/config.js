25aab98d9358ef030b40feac90d8944f33a3cd13
韩启航 <1878806686@qq.com>
2021-11-22 10:43:55 +0800

    路由添加

--- src/router/config.js
+++ src/router/config.js
@@ -59,7 +59,7 @@ const options = {
                   path: "detail",
                   name: "首页详情",
                   meta: {
-                    invisible: true  // 不显示在左侧菜单
+                    invisible: true // 不显示在左侧菜单
                   },
                   component: () => import("@/pages/home/index/detail")
                 },
@@ -118,6 +118,134 @@ const options = {
                 {
                   path: "list",
                   name: "企业列表",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () =>
+                    import("@/pages/channel/enterpriseAdmin/list")
+                }
+              ]
+            }
+          ]
+        },
+        // 财务
+        {
+          path: "/finance",
+          name: "财务",
+          component: PageView,
+          children: [
+            {
+              path: "index",
+              name: "收支账簿",
+              meta: {
+                icon: "home"
+              },
+              component: BlankView,
+              children: [
+                {
+                  path: "order",
+                  name: "订单管理",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/list")
+                },
+                {
+                  path: "detailed",
+                  name: "财务明细",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/price")
+                },
+                {
+                  path: "recharge",
+                  name: "充值记录",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/price")
+                }
+              ]
+            },
+            {
+              path: "examine",
+              name: "财务审核",
+              meta: {
+                icon: "home"
+              },
+              component: BlankView,
+              children: [
+                {
+                  path: "payment",
+                  name: "人工入款",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/list")
+                },
+                {
+                  path: "remittance",
+                  name: "线下汇款",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/price")
+                },
+                {
+                  path: "invoice",
+                  name: "发票管理",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/price")
+                },
+                {
+                  path: "contract",
+                  name: "合同管理",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/list")
+                },
+                {
+                  path: "withdrawal",
+                  name: "提现管理",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/price")
+                },
+                {
+                  path: "refund",
+                  name: "退款管理",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () => import("@/pages/channel/channelAdmin/price")
+                }
+              ]
+            },
+            {
+              path: "report",
+              name: "财务报表",
+              meta: {
+                icon: "home"
+              },
+              component: BlankView,
+              children: [
+                {
+                  path: "reconciliation",
+                  name: "渠道对账表",
+                  meta: {
+                    icon: "home"
+                  },
+                  component: () =>
+                    import("@/pages/channel/enterpriseAdmin/list")
+                },
+                {
+                  path: "income",
+                  name: "收入分摊表",
                   meta: {
                     icon: "home"
                   },
