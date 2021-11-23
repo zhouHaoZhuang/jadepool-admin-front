b8f6e144add3d21a98c23f77ff9777876b548429
18703508633 <1092231350@qq.com>
2021-11-23 10:14:40 +0800

    修改内容

--- src/pages/production/supplier/list/list.vue
+++ src/pages/production/supplier/list/list.vue
@@ -1,36 +1,63 @@
 <template>
   <div>
     <!-- 头信息 -->
-    <div class="box-wrap">
-      <div class="btn1">
-        <a-button type="primary" @click="addChannel">+ 新建供应商</a-button>
+    <div class="channel-list-container">
+      <div class="public-header-wrap">
+        <a-form-model layout="inline" :model="listQuery">
+          <a-form-model-item>
+            <a-button type="primary" icon="plus" @click="addChannel">
+              新建供应商
+            </a-button>
+          </a-form-model-item>
+          <a-form-model-item>
+            <a-select
+              style="width:120px"
+              allowClear
+              v-model="listQuery.id"
+              placeholder="供应商ID"
+            >
+              <a-select-option :value="1">
+                Jack
+              </a-select-option>
+              <a-select-option :value="2">
+                TOM
+              </a-select-option>
+            </a-select>
+          </a-form-model-item>
+          <a-form-model-item>
+            <a-input v-model="listQuery.search" placeholder="搜索关键词" />
+          </a-form-model-item>
+          <a-form-model-item>
+            <a-button type="primary" @click="search">
+              查询
+            </a-button>
+          </a-form-model-item>
+        </a-form-model>
       </div>
-      <div class="xiala">
-        <a-dropdown>
-          <a-menu slot="overlay" @click="handleMenuClick">
-            <a-menu-item key="1">
-              <a-icon type="user" />1st menu item
-            </a-menu-item>
-            <a-menu-item key="2">
-              <a-icon type="user" />2nd menu item
-            </a-menu-item>
-            <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
-          </a-menu>
-          <a-button style="margin-left: 8px">
-            供应商ID <a-icon type="down" />
-          </a-button>
-        </a-dropdown>
-      </div>
-      <div class="inpu">
-        <a-input placeholder="搜索关键字" />
-      </div>
-      <div class="btn2">
-        <a-button type="primary">查询</a-button>
-      </div>
-      <!-- 表单 -->
-      <div class="biao">
-        <a-table :columns="columns" :data-source="data">
-          <a slot="name" slot-scope="text">{{ text }}</a>
+      <div class="public-table-wrap">
+        <a-table
+          :columns="columns"
+          :data-source="data"
+          rowKey="id"
+          :pagination="paginationProps"
+        >
+          <span slot="web" slot-scope="text" style="color:#1890ff">
+            {{ text }}
+          </span>
+          <div class="status" slot="status" slot-scope="text">
+            <div v-if="text === 200" class="dot"></div>
+            <div v-else class="dot dot-err"></div>
+            {{ text === 200 ? "正常" : "冻结" }}
+          </div>
+          <span slot="action" slot-scope="text, record">
+            <a-button type="link" @click="goDetail(record)">
+              编辑
+            </a-button>
+            <a-divider type="vertical" />
+            <a-button type="link" @click="handleFrozen(record)">
+              删除
+            </a-button>
+          </span>
         </a-table>
       </div>
     </div>
@@ -38,84 +65,110 @@
 </template>
 
 <script>
-const columns = [
-  {
-    title: '供应商ID',
-    dataIndex: 'name',
-    key: 'name',
-    scopedSlots: { customRender: 'name' },
-    width:100
-  },
-  {
-    title: '全称',
-    dataIndex: 'age',
-    key: 'age',
-    width: 200,
-  },
-  {
-    title: '简称',
-    dataIndex: 'address',
-    key: 'address 1',
-    ellipsis: true,
-    width:80,
-  },
-  {
-    title: '网址',
-    dataIndex: 'address',
-    key: 'address 2',
-    ellipsis: true,
-  },
-  {
-    title: '状态',
-    dataIndex: 'address',
-    key: 'address 3',
-    ellipsis: true,
-  },
-  {
-    title: '创建时间',
-    dataIndex: 'address',
-    key: 'address 4',
-    ellipsis: true,
-  },
-  {
-    title:'操作',
-    dataIndex:'address',
-    key:'address 5',
-    ellipsis:true
-  }
-];
-
-const data = [
-  {
-    key: '1',
-    name: 'S00001',
-    age: '阿里云计算有限公司',
-    address: '阿里云',
-    tags: ['nice', 'developer'],
-  },
-  {
-    key: '2',
-    name: 'S00001',
-    age: '阿里云计算有限公司',
-    address: '阿里云',
-    tags: ['loser'],
-  },
-  {
-    key: '3',
-    name: 'S00001',
-    age: '阿里云计算有限公司',
-    address: '阿里云',
-    tags: ['cool', 'teacher'],
-  },
-];
 export default {
-  name: "index",
-  data() {
+ data() {
     return {
-      data,columns,
+      listQuery: {
+        id: undefined,
+        search: "",
+        pageNo: 1,
+        pageSize: 10,
+        total: 500
+      },
+      columns: [
+        {
+          title: "供应商ID",
+          dataIndex: "id",
+          key: "id"
+        },
+        {
+          title: "全称",
+          dataIndex: "name",
+          key: "name"
+        },
+        {
+          title: "简称",
+          dataIndex: "name1",
+          key: "name1"
+        },
+        {
+          title: "网址",
+          dataIndex: "web",
+          key: "web",
+          scopedSlots: { customRender: "web" }
+        },
+        {
+          title: "状态",
+          dataIndex: "status",
+          key: "status",
+          scopedSlots: { customRender: "status" }
+        },
+        {
+          title: "创建时间",
+          dataIndex: "gmtCreate"
+        },
+        {
+          title: "操作",
+          key: "action",
+          scopedSlots: { customRender: "action" }
+        }
+      ],
+      data: [
+        {
+          id: "0001",
+          name: "阿里云计算有限公司",
+          name1: "阿里云",
+          web: "www.aliyun.com",
+          status: 200,
+          gmtCreate: "2016-09-21  08:50:08"
+        }
+      ],
+      paginationProps: {
+        showQuickJumper: true,
+        showSizeChanger: true,
+        total: 1,
+        showTotal: (total, range) =>
+          `共 ${total} 条记录 第 ${this.listQuery.pageNo} / ${Math.ceil(
+            total / this.listQuery.pageSize
+          )} 页`,
+        onChange: this.quickJump,
+        onShowSizeChange: this.onShowSizeChange
+      }
     };
   },
   methods: {
+    // 查询
+    search() {
+      // this.getList();
+    },
+    // 查询表格数据
+    getList() {
+      this.$store.dispatch("").then(res => {
+        console.log("获取数据", res);
+        this.paginationProps.total = 500;
+      });
+    },
+    // 表格分页快速跳转n页
+    quickJump(pageNo) {
+      this.listQuery.pageNo = pageNo;
+      // this.getList();
+    },
+    // 表格分页切换每页条数
+    onShowSizeChange(current, pageSize) {
+      this.listQuery.pageNo = current;
+      this.listQuery.pageSize = pageSize;
+      // this.getList();
+    },
+    // 查看
+    goDetail(record) {
+      this.$router.push({
+        path: "/channel/index/detail",
+        query: { id: record.id }
+      });
+    },
+    // 冻结
+    handleFrozen(record) {},
+    // 新增渠道
     addChannel() {
       this.$router.push("/production/supplier/add");
     }
@@ -124,35 +177,25 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.box-wrap {
-  width: 1220px;
-  height: 615px;
-  background-color: rgba(255, 255, 255, 1);
-  position: relative;
-  .btn1 {
-    position: absolute;
-    top: 15px;
-    left: 15px;
-  }
-  .xiala {
-    position: absolute;
-    top: 15px;
-    left: 180px;
-  }
-  .inpu {
-    position: absolute;
-    top: 15px;
-    left: 310px;
-  }
-  .btn2 {
-    position: absolute;
-    top: 15px;
-    left: 510px;
-  }
-  .biao {
-    position: absolute;
-    top: 60px;
-    left: 15px;
+.channel-list-container {
+  background: #fff;
+  padding: 20px;
+  margin: 0 24px;
+  .public-table-wrap {
+    .status {
+      display: flex;
+      align-items: center;
+      .dot {
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: green;
+        margin-right: 5px;
+      }
+      .dot-err {
+        background: red;
+      }
+    }
   }
 }
 </style>
