ad0ef40e44269a9dc04a0ec8c94eb971a59ff456
韩启航 <1878806686@qq.com>
2021-11-25 10:49:06 +0800

    修改下拉选择逻辑

--- src/pages/channel/channelAdmin/list.vue
+++ src/pages/channel/channelAdmin/list.vue
@@ -12,20 +12,20 @@
             <a-select
               style="width:120px"
               allowClear
-              v-model="listQuery.id"
-              placeholder="渠道商ID"
+              v-model="listQuery.key"
+              placeholder="请选择"
             >
               <a-select-option
-                v-for="item in data"
-                :key="item.id"
-                :value="item.id"
+                v-for="item in columns.slice(0, columns.length - 1)"
+                :key="item.dataIndex"
+                :value="item.dataIndex"
               >
-                {{ item.cutomerName }}
+                {{ item.title }}
               </a-select-option>
             </a-select>
           </a-form-model-item>
           <a-form-model-item>
-            <a-input v-model="listQuery.cutomerName" placeholder="搜索关键词" />
+            <a-input v-model="listQuery.search" placeholder="搜索关键词" />
           </a-form-model-item>
           <a-form-model-item>
             <a-button type="primary" @click="search">
@@ -74,8 +74,8 @@ export default {
   data() {
     return {
       listQuery: {
-        id: undefined,
-        cutomerName: "",
+        key: undefined,
+        search: "",
         currentPage: 1,
         pageSize: 10,
         total: 0
@@ -151,7 +151,10 @@ export default {
     getList() {
       this.tableLoading = true;
       this.$store
-        .dispatch("channel/getList", this.listQuery)
+        .dispatch("channel/getList", {
+          ...this.listQuery,
+          [this.listQuery.key]: this.listQuery.search
+        })
         .then(res => {
           this.data = [...res.data.list];
           this.paginationProps.total = res.data.totalCount * 1;
--- src/pages/channel/channelAdmin/price.vue
+++ src/pages/channel/channelAdmin/price.vue
@@ -23,23 +23,20 @@
             <a-select
               style="width:120px"
               allowClear
-              v-model="listQuery.productCode"
-              placeholder="产品名称"
+              v-model="listQuery.key"
+              placeholder="请选择"
             >
               <a-select-option
-                v-for="item in data"
-                :key="item.productCode"
-                :value="item.id"
+                v-for="item in columns.slice(0, columns.length - 1)"
+                :key="item.dataIndex"
+                :value="item.dataIndex"
               >
-                {{ item.productName }}
+                {{ item.title }}
               </a-select-option>
             </a-select>
           </a-form-model-item>
           <a-form-model-item>
-            <a-input
-              v-model="listQuery.channelCustomerName"
-              placeholder="搜索关键词"
-            />
+            <a-input v-model="listQuery.search" placeholder="搜索关键词" />
           </a-form-model-item>
           <a-form-model-item>
             <a-button type="primary" @click="search">
@@ -82,8 +79,8 @@ export default {
     return {
       channelPriceType,
       listQuery: {
-        channelCustomerName: "",
-        productCode: undefined,
+        key: undefined,
+        search: "",
         currentPage: 1,
         pageSize: 10,
         total: 0
@@ -155,7 +152,10 @@ export default {
     getList() {
       this.tableLoading = true;
       this.$store
-        .dispatch("channel/getPriceList", this.listQuery)
+        .dispatch("channel/getPriceList", {
+          ...this.listQuery,
+          [this.listQuery.key]: this.listQuery.search
+        })
         .then(res => {
           this.data = [...res.data.list];
           this.paginationProps.total = res.data.totalCount * 1;
