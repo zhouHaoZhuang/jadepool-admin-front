52574cb556a3dd6da1f1f5a0353f1122523ca89f
韩启航 <1878806686@qq.com>
2021-11-29 15:19:50 +0800

    登录接口

--- src/config/env.js
+++ src/config/env.js
@@ -1,13 +1,18 @@
 let VUE_APP_BASE_URL = "";
+let appId = "619c93dc69a93fbb8a1faf8c";
+let appHost = "https://resource-poor.authing.cn";
 
 if (process.env.VUE_APP_ENV === "dev") {
-  VUE_APP_BASE_URL = 'http://8.136.205.83:8080';
+  VUE_APP_BASE_URL = "http://8.136.205.83:8080";
+  // VUE_APP_BASE_URL = "http://127.0.0.1:8080";
 } else if (process.env.VUE_APP_ENV === "test") {
   VUE_APP_BASE_URL = "http://api-test.com";
 } else if (process.env.VUE_APP_ENV === "prod") {
-  VUE_APP_BASE_URL = 'https://api.chadian.com';
+  VUE_APP_BASE_URL = "https://api.chadian.com";
 }
 
 export default {
-  VUE_APP_BASE_URL
+  VUE_APP_BASE_URL,
+  appId,
+  appHost
 };
