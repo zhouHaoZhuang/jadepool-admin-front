7bcea00959b3f42631585d112aff18ab58b9387e
zhouHaoZhuang <88356815+zhouHaoZhuang@users.noreply.github.com>
2021-12-30 10:51:39 +0800

    修改请求

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ let appId = "619c93dc69a93fbb8a1faf8c";
 let appHost = "https://resource-poor.authing.cn";
 
 if (process.env.VUE_APP_ENV === "dev") {
-  VUE_APP_BASE_URL = "http://8.136.205.83:8080";
+  VUE_APP_BASE_URL = "http://rps.test.ydidc.com";
   // VUE_APP_BASE_URL = "http://127.0.0.1:8080";
 } else if (process.env.VUE_APP_ENV === "test") {
   VUE_APP_BASE_URL = "http://8.136.205.83:8080";
