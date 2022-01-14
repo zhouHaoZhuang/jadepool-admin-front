21709a8b169e8033d8e675b9d048c5c25f130728
韩启航 <1878806686@qq.com>
2022-01-14 17:43:42 +0800

    登陆页面优化

--- src/layouts/CommonLayout.vue
+++ src/layouts/CommonLayout.vue
@@ -3,25 +3,48 @@
     <div class="content"><slot></slot></div>
     <!-- 底部 -->
     <!-- <page-footer :link-list="footerLinks" :copyright="copyright"></page-footer> -->
+    <div class="cloud-img"></div>
+    <div class="footer">
+      Copyright 2021 © 浙江云盾互联网科技有限公司 浙ICP备2021017872号-1
+    </div>
   </div>
 </template>
 
 <script>
 // import PageFooter from '@/layouts/footer/PageFooter'
-import {mapState} from 'vuex'
+import { mapState } from "vuex";
 
 export default {
-  name: 'CommonLayout',
+  name: "CommonLayout",
   // components: {PageFooter},
   computed: {
-    ...mapState('setting', ['footerLinks', 'copyright'])
+    ...mapState("setting", ["footerLinks", "copyright"])
   }
-}
+};
 </script>
 
 <style scoped lang="less">
-.common-layout{
-  background: #ccc;
+.common-layout {
   height: 100vh;
+  position: relative;
+  .cloud-img {
+    width: 100%;
+    height: 330px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    background: url("../assets/img/passport/login_cloud.png") no-repeat center;
+    background-size: cover;
+  }
+  .footer {
+    width: 100%;
+    text-align: center;
+    color: #ccc;
+    height: 50px;
+    line-height: 50px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+  }
 }
 </style>
--- src/pages/passport/login.vue
+++ src/pages/passport/login.vue
@@ -1,26 +1,26 @@
 <template>
-  <common-layout class="login-wrap">
+  <common-layout class="login-container">
     <div class="login">
       <a-form-model ref="ruleForm" :model="form" :rules="rules">
         <a-form-model-item prop="username">
-          <a-input v-model="form.username" size="large" placeholder="账户">
+          <a-input v-model="form.username" placeholder="账户">
             <a-icon slot="prefix" type="user" />
           </a-input>
         </a-form-model-item>
         <a-form-model-item prop="password">
-          <a-input
-            v-model="form.password"
-            size="large"
-            placeholder="密码"
-            type="password"
-          >
+          <a-input v-model="form.password" placeholder="密码" type="password">
             <a-icon slot="prefix" type="lock" />
           </a-input>
         </a-form-model-item>
         <a-form-model-item>
-          <a-button type="primary" :loading="loading" @click="onSubmit"
-            >登录</a-button
+          <a-button
+            style="width:100%"
+            type="primary"
+            :loading="loading"
+            @click="onSubmit"
           >
+            登录
+          </a-button>
         </a-form-model-item>
       </a-form-model>
     </div>
@@ -79,12 +79,44 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.login-wrap {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+.login-container {
+  background: #26a6f5;
   .login {
-    width: 600px;
+    width: 460px;
+    // height: 320px;
+    height: 270px;
+    border-radius: 4px;
+    padding: 30px 20px 40px;
+    padding-top: 50px;
+    background: #fff;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 999;
+  }
+}
+</style>
+<style lang="less">
+.beauty-scroll {
+  overflow-y: auto !important;
+}
+.login-container {
+  .login-btn {
+    margin-bottom: 10px;
+  }
+  .code-wrap,
+  .btn-box {
+    .ant-form-item-children {
+      display: flex;
+      justify-content: space-between;
+      .btn1 {
+        padding-left: 0;
+      }
+      .btn2 {
+        padding-right: 0;
+      }
+    }
   }
 }
 </style>
