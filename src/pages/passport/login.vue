9480109e7a0a829b1a31583a68f62fcad7a78cd7
李钰 <yu.li@yfmail.cn>
2022-03-16 18:53:13 +0800

    首页注册账号 ，忘记密码以及登录加图片验证

--- src/pages/passport/login.vue
+++ src/pages/passport/login.vue
@@ -1,100 +1,82 @@
 <template>
   <common-layout class="login-container">
-    <!-- 左上角logo -->
-    <div class="logo-wrap">
-      <div class="txt">南天门</div>
-      <div class="line"></div>
-      <div class="txt">ntm.slayun.com</div>
-    </div>
     <div class="login-wrap">
       <div class="img-bg"></div>
       <div class="login">
-        <!-- <div class="tabs-wrap">
+        <div class="tabs-wrap">
           <div class="tabs-item">
-            账号密码登录
+            登录
           </div>
-        </div> -->
-        <a-tabs default-active-key="1" @change="callback">
-          <a-tab-pane key="1" tab="账号密码登录">
-            <a-form-model ref="ruleForm" :model="form" :rules="rules">
-              <a-form-model-item prop="username">
-                <a-input
-                  v-model="form.username"
-                  placeholder="请输入账号"
-                  size="large"
-                >
-                  <a-icon slot="prefix" type="user" />
-                </a-input>
-              </a-form-model-item>
+        </div>
 
-              <a-form-model-item prop="password">
-                <a-input
-                  v-model="form.password"
-                  placeholder="请输入密码"
-                  type="password"
-                  size="large"
-                  @pressEnter="onSubmit"
-                >
-                  <a-icon slot="prefix" type="lock" />
-                </a-input>
-              </a-form-model-item>
-            </a-form-model>
-          </a-tab-pane>
-          <!-- <a-tab-pane key="2" tab="短信验证码登录" force-render> -->
-          <a-tab-pane key="2" tab="" force-render disabled>
-            <a-form-model ref="ruleForm" :model="form" :rules="rules">
-              <a-form-model-item prop="phone">
-                <a-input
-                  v-model="form.phone"
-                  addon-before="+86"
-                  placeholder="11位手机号"
-                  v-number-evolution
-                  :max-length="11"
-                  size="large"
-                />
-              </a-form-model-item>
-              <a-form-model-item prop="verificationCode">
-                <a-input
-                  type="text"
-                  style="width:200px"
-                  size="large"
-                  v-model="form.verificationCode"
-                  placeholder="请进行图片验证"
-                  :max-length="6"
-                />
-                <div @click="refreshCode()" class="code" title="点击切换验证码">
-                  <Identify :identifyCode="identifyCode" />
-                </div>
-              </a-form-model-item>
-              <a-form-model-item class="code-wrap" prop="code">
-                <a-input
-                  v-model="form.code"
-                  style="width:200px"
-                  placeholder="输入验证码"
-                  v-number-evolution
-                  :max-length="6"
-                  size="large"
-                >
-                  <a-icon slot="prefix" type="smile" />
-                </a-input>
-                <CodeBtn
-                  :phone="form.phone"
-                  size="large"
-                  @validate="validateImgCode"
-                />
-              </a-form-model-item>
-            </a-form-model>
-          </a-tab-pane>
-        </a-tabs>
-        <a-button
-          class="login-btn"
-          type="primary"
-          size="large"
-          :loading="loading"
-          @click="onSubmit"
-        >
-          登录
-        </a-button>
+        <a-form-model ref="ruleForm" :model="form" :rules="rules">
+          <a-form-model-item prop="username">
+            <a-input
+              v-model="form.username"
+              placeholder="请输入账号"
+              size="large"
+            >
+              <a-icon slot="prefix" type="user" />
+            </a-input>
+          </a-form-model-item>
+
+          <a-form-model-item prop="password">
+            <a-input
+              v-model="form.password"
+              placeholder="请输入密码"
+              type="password"
+              size="large"
+              @pressEnter="onSubmit"
+            >
+              <a-icon slot="prefix" type="lock" />
+            </a-input>
+          </a-form-model-item>
+          <a-form-model-item prop="verificationCode">
+            <a-input
+              type="text"
+              style="width:200px"
+              size="large"
+              v-model="form.verificationCode"
+              placeholder="请输入图片验证码"
+              :max-length="6"
+            >
+            <a-icon slot="prefix" type="safety-certificate" />
+            </a-input>
+            <div @click="refreshCode()" class="code" title="点击切换验证码">
+              <Identify :identifyCode="identifyCode" />
+            </div>
+          </a-form-model-item>
+          <a-form-model-item>
+            <a-button
+              class="login-btn"
+              type="primary"
+              size="large"
+              :loading="loading"
+              @click="onSubmit"
+            >
+              登录
+            </a-button>
+          </a-form-model-item>
+
+          <a-form-model-item class="btn-box">
+            <a-button
+              size="large"
+              class="btn1"
+              type="link"
+              @click="resetPassword"
+            >
+              忘记密码
+            </a-button>
+            <a-button
+              size="large"
+              class="btn2"
+              type="link"
+              @click="handleJumpRegister"
+            >
+              免费注册
+            </a-button>
+          </a-form-model-item>
+        </a-form-model>
       </div>
     </div>
   </common-layout>
@@ -102,20 +84,17 @@
 
 <script>
 import CommonLayout from "@/layouts/CommonLayout";
-import CodeBtn from "@/components/CodeBtn/index";
 import Identify from "@/components/Identify";
 import { getRandomCode } from "@/utils/index";
 export default {
   name: "Login",
-  components: { CommonLayout, CodeBtn, Identify },
+  components: { CommonLayout, Identify },
   data() {
     return {
       loginStatus: "pwd",
       form: {
         username: "",
         password: "",
-        code: "",
-        phone: "",
         verificationCode: ""
       },
       rules: {
@@ -133,24 +112,10 @@ export default {
             trigger: "blur"
           }
         ],
-        phone: [
-          {
-            required: true,
-            message: "请输入手机号",
-            trigger: ["blur", "change"]
-          }
-        ],
-        code: [
-          {
-            required: true,
-            message: "请输入验证码",
-            trigger: ["blur", "change"]
-          }
-        ],
         verificationCode: [
           {
             required: true,
-            message: "请输入图片图片校验码",
+            message: "请输入图片校验码",
             trigger: ["blur", "change"]
           },
           {
@@ -173,29 +138,11 @@ export default {
   },
   methods: {
     onSubmit() {
-      let count = 0;
-      const pwdArr = ["username", "password"];
-      const codeArr = ["phone", "code", "verificationCode"];
-      // 密码登录
-      if (this.loginStatus == "pwd") {
-        let isAllowNext = false; // 是否允许下一步
-        pwdArr.forEach(item => {
-          this.$refs.ruleForm.validateField(item, errorMsg => {
-            // 返回值为空时，验证通过；返回值非空时，验证失败
-            if (errorMsg) return;
-          });
-          // 如果某个字段校验通过则计数+1
-          ++count;
-        });
-        console.log(count, "count");
-        count === pwdArr.length ? (isAllowNext = true) : "";
-        if (isAllowNext) {
+      this.$refs.ruleForm.validate(valid => {
+        if (valid) {
           this.loading = true;
-          let newObj = {};
-          newObj.username = this.form.username;
-          newObj.password = this.form.password;
           this.$store
-            .dispatch("user/login", newObj)
+            .dispatch("user/login", this.form)
             .then(res => {
               this.$router.push("/");
             })
@@ -203,33 +150,7 @@ export default {
               this.loading = false;
             });
         }
-      }
-      // 手机验证码登录
-      if (this.loginStatus == "code") {
-        let isAllowNext = false;
-        codeArr.forEach(item => {
-          this.$refs.ruleForm.validateField(item, errorMsg => {
-            if (errorMsg) return;
-          });
-          ++count;
-        });
-        count === codeArr.length ? (isAllowNext = true) : "";
-        if (isAllowNext) {
-          let newObj = {};
-          newObj.phone = this.form.phone;
-          newObj.code = this.form.code;
-          newObj.verificationCode = this.form.verificationCode;
-          // this.loading = true;
-          // this.$store
-          //   .dispatch("user/login", newObj)
-          //   .then(res => {
-          //     this.$router.push("/");
-          //   })
-          //   .finally(() => {
-          //     this.loading = false;
-          //   });
-        }
-      }
+      });
     },
     // 获取验证码组件校验图形验证
     validateImgCode(callback) {
@@ -244,14 +165,13 @@ export default {
     refreshCode() {
       this.identifyCode = getRandomCode();
     },
-    //切换登录方式
-    callback(key) {
-      console.log(key);
-      if (key == 1) {
-        this.loginStatus = "pwd";
-      } else {
-        this.loginStatus = "code";
-      }
+    // 跳转注册
+    handleJumpRegister() {
+      this.$router.push("/register");
+    },
+    // 跳转重置密码
+    resetPassword() {
+      this.$router.push("/resetpassword");
     }
   }
 };
@@ -261,23 +181,12 @@ export default {
 .login-container {
   background: #406ad3;
   position: relative;
-  .logo-wrap {
-    position: absolute;
-    top: 60px;
-    left: 100px;
-    display: flex;
-    align-items: center;
-    .txt {
-      font-size: 28px;
-      color: #fff;
-      font-weight: 500px;
-    }
-    .line {
-      width: 2px;
-      height: 35px;
-      background: #fff;
-      margin: 0 10px;
-    }
+  .img-bg {
+    flex: 1;
+    height: 450px;
+    background: url("../../assets/img/passport/login.jpg") no-repeat center;
+    background-size: cover;
+    margin-right: 100px;
   }
   .login-wrap {
     width: 1200px;
@@ -287,16 +196,10 @@ export default {
     align-items: center;
     justify-content: space-between;
   }
-  .img-bg {
-    flex: 1;
-    height: 400px;
-    background: url("../../assets/img/passport/login.jpg") no-repeat center;
-    background-size: cover;
-    margin-right: 100px;
-  }
+
   .login {
     width: 410px;
-    height: 400px;
+    height: 450px;
     border-radius: 5px;
     border-radius: 4px;
     padding: 40px;
@@ -318,7 +221,7 @@ export default {
           content: "";
           display: block;
           margin: 3px auto 0;
-          width: 90%;
+          width: 100%;
           height: 2px;
           background: #406ad3;
         }
