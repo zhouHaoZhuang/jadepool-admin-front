6ca26ac14c4a069c354dd7dc12c6a2e7fe4911bc
韩启航 <1878806686@qq.com>
2022-03-21 18:45:54 +0800

    新增产品调整

--- src/pages/production/product/information/addproduct.vue
+++ src/pages/production/product/information/addproduct.vue
@@ -15,13 +15,6 @@
         >
           <a-input v-model="form.productName" />
         </a-form-model-item>
-        <!-- <a-form-model-item
-          ref="productCode"
-          label="资源池产品CODE"
-          prop="productCode"
-        >
-          <a-input v-model="form.productCode" />
-        </a-form-model-item> -->
         <a-form-model-item label="供应商" prop="supplierCode">
           <a-select
             label-in-value
@@ -39,25 +32,76 @@
         </a-form-model-item>
         <a-form-model-item ref="supplierProductCode" label="供应商产品CODE">
           <a-input v-model="form.supplierProductCode" />
+          <div class="info-txt">
+            输入规范：产品简称_厂商简称，例CND_ALI
+          </div>
         </a-form-model-item>
-        <a-form-model-item ref="supplierProductType" label="供应商产品Type">
-          <a-input v-model="form.supplierProductType" />
+        <a-form-model-item
+          ref="supplierProductType"
+          label="产品分类"
+          prop="type"
+        >
+          <a-radio-group v-model="form.type">
+            <a-radio :value="1">
+              无
+            </a-radio>
+            <a-radio :value="2">
+              有
+            </a-radio>
+          </a-radio-group>
         </a-form-model-item>
-        <a-form-model-item label="默认采购账号" prop="defaultPurchaseAccount">
-          <a-select v-model="form.defaultPurchaseAccount">
-            <a-select-option
-              v-for="v in purchase"
-              :value="v.accountCode"
-              :key="v.id"
+        <!-- 产品分类列表 -->
+        <div v-if="form.type === 2">
+          <div
+            class="product-list"
+            v-for="(item, index) in form.typeList"
+            :key="item.id"
+          >
+            <a-form-model-item
+              :wrapper-col="{ span: 18, offset: 6 }"
+              :prop="'typeList.' + index + '.name'"
+              :rules="{
+                required: true,
+                message: '请输入分类名称',
+                trigger: ['blur', 'change']
+              }"
             >
-              {{ v.accountTag }}
-            </a-select-option>
-          </a-select>
-        </a-form-model-item>
-
-        <a-form-model-item ref="pm" label="产品经理">
-          <a-input v-model="form.pm" />
-        </a-form-model-item>
+              <div class="item">
+                <div class="label">分类名称：</div>
+                <div class="value">
+                  <a-input v-model="item.name" :maxLength="20" />
+                </div>
+                <a-icon
+                  v-if="index === 0"
+                  class="add"
+                  type="plus-square"
+                  @click="handleAdd"
+                />
+                <a-icon
+                  v-else
+                  class="del"
+                  type="minus-square"
+                  @click="handleDel(index)"
+                />
+              </div>
+            </a-form-model-item>
+            <a-form-model-item :wrapper-col="{ span: 18, offset: 6 }">
+              <div class="item">
+                <div class="label">最小计费单位：</div>
+                <div class="value">
+                  <a-select v-model="item.unit">
+                    <a-select-option :value="1">
+                      万次
+                    </a-select-option>
+                    <a-select-option :value="2">
+                      GB
+                    </a-select-option>
+                  </a-select>
+                </div>
+              </div>
+            </a-form-model-item>
+          </div>
+        </div>
         <a-form-model-item label="备注" ref="remark">
           <a-input v-model="form.remark" type="textarea" />
         </a-form-model-item>
@@ -78,24 +122,23 @@ export default {
       other: "",
       form: {
         productName: "", //产品名称
-        defaultPurchaseAccount: "", //默认采购账号
         productCode: "", //产品CODE
         supplierCode: "", //供应商CODE
         supplierName: "", //供应商
         supplierProductCode: "", //供应商产品CODE
         supplierProductType: "", //供应商产品Type
-        pm: "", //产品经理
-        remark: "" //备注
+        remark: "", //备注
+        type: 1,
+        typeList: [
+          {
+            id: -1,
+            name: "",
+            unit: 1
+          }
+        ]
       },
       rules: {
         productName: [
-          {
-            required: true,
-            message: "输入值不能为空",
-            trigger: "blur"
-          }
-        ],
-        defaultPurchaseAccount: [
           {
             required: true,
             message: "输入值不能为空",
@@ -113,7 +156,14 @@ export default {
           {
             required: true,
             message: "输入值不能为空",
-            trigger: "blur"
+            trigger: "change"
+          }
+        ],
+        type: [
+          {
+            required: true,
+            message: "请选择产品分类",
+            trigger: "change"
           }
         ]
       },
@@ -134,9 +184,23 @@ export default {
     });
   },
   methods: {
+    // 添加产品分类
+    handleAdd() {
+      const id = this.form.typeList[this.form.typeList.length - 1].id - 1;
+      const data = {
+        id,
+        name: "",
+        unit: 1
+      };
+      this.form.typeList.push({ ...data });
+    },
+    // 删除产品分类
+    handleDel(index) {
+      this.form.typeList.splice(index, 1);
+    },
     // 提交
     onSubmit() {
-      this.form.supplierCode = this.form.supplierCode.key;
+      // this.form.supplierCode = this.form.supplierCode.key;
       // console.log(this.form);
       this.$refs.ruleForm.validate(valid => {
         if (valid) {
@@ -177,7 +241,7 @@ export default {
   background-color: #fff;
   margin: 0 24px;
   padding: 20px;
-  height: 615px;
+  min-height: 615px;
   > .box-wrap {
     width: 600px;
     margin: 0 auto;
@@ -186,6 +250,42 @@ export default {
     button {
       position: relative;
       left: 150px;
+    }
+  }
+  .info-txt{
+    color: #aaa;
+    line-height: 20px;
+  }
+  .product-list {
+    .item {
+      display: flex;
+      position: relative;
+      .label {
+        width: 120px;
+        &::before {
+          display: inline-block;
+          margin-right: 4px;
+          color: #f5222d;
+          font-size: 14px;
+          font-family: SimSun, sans-serif;
+          line-height: 1;
+          content: "*";
+        }
+      }
+      .value {
+        flex: 1;
+        padding-right: 30px;
+      }
+      .add,
+      .del {
+        position: absolute;
+        right: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        font-size: 24px;
+        color: #aaa;
+        cursor: pointer;
+      }
     }
   }
 }
