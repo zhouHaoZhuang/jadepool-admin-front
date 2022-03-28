63c3947fb3f3329961fc6fb73757c64352d26358
李钰 <yu.li@yfmail.cn>
2022-03-28 17:39:04 +0800

    图形验证码不区分大小写

--- src/pages/passport/resetpassword.vue
+++ src/pages/passport/resetpassword.vue
@@ -168,7 +168,7 @@ export default {
           },
           {
             validator: (rule, value, callback) => {
-              if (value !== this.identifyCode) {
+              if (value.toLowerCase() !== this.identifyCode.toLowerCase()) {
                 callback(new Error("图形验证码不正确"));
               }
               callback();
